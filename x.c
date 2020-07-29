2205d654ae44caf5986ef7959801bf6ff3d1fe0d
dosisod <39638017+dosisod@users.noreply.github.com>
2020-07-28 20:45:52 -0700

    added back opacity

--- x.c
+++ x.c
@@ -810,7 +810,7 @@ xloadcols(void)
 		}
 
 	/* set alpha value of bg color */
-	float alpha = 1;
+	float alpha = 0.8;
 	if (opt_alpha)
 		alpha = strtof(opt_alpha, NULL);
 	dc.col[defaultbg].color.alpha = (unsigned short)(0xffff * alpha);
